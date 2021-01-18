1a4c04ac3ae3ef2c9f5a9558e23bf3fa343fa646
Precious Alexandra Kanu <kanuprecious89@gmail.com>
2021-01-18 01:00:20 +0100

    debug added to local tracks

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -45,130 +45,6 @@ export default {
   mounted() {
     this.startVideoChat();
   },
-  // mounted() {
-  //   connect(this.token, {
-  //     video: true,
-  //     audio: true,
-  //     name: this.room, //change to room
-  //     // logLevel: "debug",
-  //   })
-  //     .then((room) => {
-  //       // create and attach local track to div
-  //       createLocalVideoTrack()
-  //         .then((track) => {
-  //           this.$refs.localVideoRef.appendChild(track.attach());
-  //         })
-  //         .catch((error) => console.log({ localVideoError: error.message }));
-
-  //       // For RemoteParticipants that are already in the Room
-  //       room.participants.forEach((participant) => {
-  //         participant.tracks.forEach((publication) => {
-  //           if (publication.track) {
-  //             const track = publication.track;
-  //             this.$refs.remoteVideoRef.appendChild(track.attach());
-  //           }
-  //         });
-
-  //         participant.on("trackSubscribed", (track) => {
-  //           this.$refs.remoteVideoRef.appendChild(track.attach());
-  //         });
-  //       });
-
-  //       room.on("participantConnected", (participant) => {
-  //         // Attach the Participant's Media to a <div> element.
-  //         participant.tracks.forEach((publication) => {
-  //           // For RemoteParticipants that join the room or that are already in the Room
-  //           if (publication.isSubscribed) {
-  //             const track = publication.track;
-  //             this.$refs.remoteVideoRef.appendChild(track.attach());
-  //             console.log("attached to remote video");
-  //           }
-  //         });
-
-  //         participant.on("trackSubscribed", (track) => {
-  //           this.$refs.remoteVideoRef.appendChild(track.attach());
-  //         });
-  //       });
-
-  //       // room.on('participantDisconnected', participant => {
-  //       //   console.log(`Participant disconnected: ${participant.identity}`);
-  //       // });
-
-  //       // // Log new Participants as they connect to the Room
-  //       // room.once('participantConnected', participant => {
-  //       //   console.log(`Participant "${participant.identity}" has connected to the Room`);
-  //       // });
-
-  //       // // Log Participants as they disconnect from the Room
-  //       // room.once('participantDisconnected', participant => {
-  //       //   console.log(`Participant "${participant.identity}" has disconnected from the Room`);
-  //       // });
-
-  //       // // mute local audio
-  //       // room.localParticipant.audioTracks.forEach(publication => {
-  //       //   publication.track.disable();
-  //       // });
-
-  //       // // mute local video
-  //       // room.localParticipant.videoTracks.forEach(publication => {
-  //       //   publication.track.disable();
-  //       // });
-
-  //       // // unmute local audio
-  //       // room.localParticipant.audioTracks.forEach(publication => {
-  //       //   publication.track.enable();
-  //       // });
-
-  //       // // unmute local video
-  //       // room.localParticipant.videoTracks.forEach(publication => {
-  //       //   publication.track.enable();
-  //       // });
-
-  //       // function handleTrackDisabled(track) {
-  //       //   track.on('disabled', () => {
-  //       //     /* Hide the associated <video> element and show an avatar image. */
-  //       //   });
-  //       // }
-
-  //       // room.participants.forEach(participant => {
-  //       //   participant.tracks.forEach(publication => {
-  //       //     if (publication.isSubscribed) {
-  //       //       handleTrackDisabled(publication.track);
-  //       //     }
-  //       //     publication.on('subscribed', handleTrackDisabled);
-  //       //   });
-  //       // });
-
-  //       // function handleTrackEnabled(track) {
-  //       //   track.on('enabled', () => {
-  //       //     /* Hide the avatar image and show the associated <video> element. */
-  //       //   });
-  //       // }
-
-  //       // room.participants.forEach(participant => {
-  //       //   participant.tracks.forEach(publication => {
-  //       //     if (publication.isSubscribed) {
-  //       //       handleTrackEnabled(publication.track);
-  //       //     }
-  //       //     publication.on('subscribed', handleTrackEnabled);
-  //       //   });
-  //       // });
-
-  //       // room.on('disconnected', room => {
-  //       //   // Detach the local media elements
-  //       //   room.localParticipant.tracks.forEach(publication => {
-  //       //     const attachedElements = publication.track.detach();
-  //       //     attachedElements.forEach(element => element.remove());
-  //       //   });
-  //       // });
-
-  //       // // To disconnect from a Room
-  //       // room.disconnect();
-  //     })
-  //     .catch((error) => {
-  //       console.log({ connectionError: error });
-  //     });
-  // },
   methods: {
     ...mapMutations(["setToken", "setSentToken"]),
     disconnect() {},
@@ -183,18 +59,14 @@ export default {
         audio: true,
         logLevel: "debug",
       }).then((room) => {
-        createLocalVideoTrack()
+        createLocalVideoTrack({logLevel: "debug"})
           .then((track) => {
             this.$refs.videoRef.appendChild(track.attach());
           })
           .catch((error) => console.log({ localVideoError: error.message }));
         room.participants.forEach(this.participantConnected);
         room.on("participantConnected", this.participantConnected);
-
         room.on("participantDisconnected", this.participantDisconnected);
-
-        // window.addEventListener("beforeunload", this.tidyUp(room));
-        // window.addEventListener("pagehide", this.tidyUp(room));
       });
     },
     participantConnected(participant) {
@@ -228,7 +100,7 @@ export default {
       this.$ref.videoRef.remove();
     },
     async leaveRoom() {
-      const tracks = await createLocalTracks();
+      const tracks = await createLocalTracks({ logLevel: "debug" });
 
       const room = await connect(this.token, {
         name: this.room,
@@ -250,15 +122,6 @@ export default {
       this.setToken("");
       this.setSentToken(false);
     },
-    // tidyUp(room, event) {
-    //   if(event.persisted) {
-    //     return;
-    //   }
-    //   if(room) {
-    //     room.disconnect();
-    //     room = null;
-    //   }
-    // }
   },
 };
 </script>
